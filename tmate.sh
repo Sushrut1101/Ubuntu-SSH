758031ee202837e290ca4996d76f529701c44c31
Sushrut1101 <guptasushrut@gmail.com>
2022-01-23 03:01:16 +0530

    tmate.sh: Reduce the sleep time
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- tmate.sh
+++ tmate.sh
@@ -2,4 +2,4 @@
 tmate -S /tmp/tmate.sock new-session -d               # Launch tmate in a headless mode
 tmate -S /tmp/tmate.sock wait tmate-ready             # Blocks until the SSH connection is established
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'    # Prints the SSH connection string
-sleep 100
+sleep 3
